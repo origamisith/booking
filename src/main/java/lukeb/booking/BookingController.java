9d3abe62af7f89c7b4067013cb5c45fe771c3db9
Luke Sedillo <lukeboarder@gmail.com>
2019-08-01 22:12:48 -0700

    Add REST controllers and database access

--- src/main/java/lukeb/booking/BookingController.java
+++ src/main/java/lukeb/booking/BookingController.java
@@ -1,12 +1,44 @@
 package lukeb.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/bookings")
 public class BookingController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello";
+
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    public BookingController(BookingRepository bookingRepository){
+        this.bookingRepository = bookingRepository;
+    }
+
+    public BookingController(){
+    }
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<HotelBooking> getAll(){
+        return bookingRepository.findAll();
+    }
+
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price) {
+        return bookingRepository.findByPricePerNightLessThan(price);
+    }
+
+    @RequestMapping(value="/create", method = RequestMethod.POST)
+    public List<HotelBooking> create(@RequestBody HotelBooking hotelBooking){
+        bookingRepository.save(hotelBooking);
+
+        return bookingRepository.findAll();
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public List<HotelBooking> remove(@PathVariable long id){
+        bookingRepository.deleteById(id);
+
+        return bookingRepository.findAll();
     }
 }
---
+++ src/main/java/lukeb/booking/DemoController.java
@@ -0,0 +1,12 @@
+package lukeb.booking;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
